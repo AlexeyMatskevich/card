256f58e04fe4d6183bf4e6bd29ad30c78a5b1223
AlexeyMatskevich <alexey.matskevich@flatstack.com>
2019-05-08 01:49:10 +0300

    Add implementation of spec

--- card.rb
+++ card.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_reader :suit, :rank
+  # attr_reader :suit, :rank
 
   def initialize(suit:, rank:)
     @suit = suit
@@ -14,8 +14,10 @@ end
 
 RSpec.describe 'a playing card' do
   it 'has a suit' do
+    raise unless Card.new(suit: :spades, rank: 4).suit == :spades
   end
 
   it 'has a rank' do
+    raise unless Card.new(suit: :spades, rank: 4).rank == 4
   end
 end
