7d7402682ce61b472c5a43a84bcfe2556f4c40a2
AlekseiMatskevich <alexey.matskevich@flatstack.com>
2019-05-08 12:36:57 +0300

    Split spec with implementation to separate file

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,18 +1,6 @@
-class Card
-  attr_reader :suit, :rank
+require 'card'
 
-  def initialize(suit:, rank:)
-    @suit = suit
-    @rank = case rank
-            when :jack then 11
-            when :queen then 12
-            when :king then 13
-            else rank
-            end
-  end
-end
-
-RSpec.describe 'a playing card' do
+describe 'a playing card' do
   it 'has a suit' do
     raise unless Card.new(suit: :spades, rank: 4).suit == :spades
   end
